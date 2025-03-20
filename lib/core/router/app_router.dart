0ddfcd33b5c213d0d638bc3f8f3eede4a55c33f3
Leandro Rodriguez <leandro.r@manadigital.com.ar>
2025-03-20 16:49:26 -0300

    router fix

--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -4,7 +4,7 @@ import 'package:ocr_app_flutter/home/presentation/home_screen.dart';
 import 'package:ocr_app_flutter/image_processing/presentation/screens/image_processing_screen.dart';
 
 final appRouter = GoRouter(
-  initialLocation: '/home',
+  initialLocation: '/login',
   routes: [
     GoRoute(path: '/login', builder: (context, state) => const LoginScreen()),
     GoRoute(path: '/home', builder: (context, state) => const HomeScreen()),
