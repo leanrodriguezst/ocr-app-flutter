ce664351cd55a2e8c5117117c89fac9c9836435f
Leandro Rodriguez <leandro.r@manadigital.com.ar>
2025-03-31 16:39:35 -0300

    open cv in progress

--- lib/image_processing/infrastructure/image_processing_service_impl.dart
+++ lib/image_processing/infrastructure/image_processing_service_impl.dart
@@ -1,6 +1,7 @@
 import 'package:google_ml_kit/google_ml_kit.dart';
 import 'package:ocr_app_flutter/core/utils/result.dart';
 import 'package:ocr_app_flutter/image_processing/data/interfaces/image_processing_service.dart';
+import 'package:opencv_core/opencv.dart' as cv;
 
 class ImageProcessingServiceImpl extends ImageProcessingService {
   final TextRecognizer _textRecognizer;
@@ -9,6 +10,10 @@ class ImageProcessingServiceImpl extends ImageProcessingService {
 
   @override
   Future<Result<String, Exception>> processImage(String imagePath) async {
+    final cv.Mat image = await cv.imreadAsync(imagePath);
+
+    final gray = await cv.cvtColorAsync(image, cv.COLOR_BGR2GRAY);
+
     final InputImage inputImage = InputImage.fromFilePath(imagePath);
     try {
       final RecognizedText recognizedText = await _textRecognizer.processImage(
