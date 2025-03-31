d4b9fad553103429200493af63e2870f69a7bcff
Leandro Rodriguez <leandro.r@manadigital.com.ar>
2025-03-31 14:20:54 -0300

    tesseract working

--- lib/image_processing/infrastructure/image_processing_service_impl.dart
+++ lib/image_processing/infrastructure/image_processing_service_impl.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_tesseract_ocr/flutter_tesseract_ocr.dart';
 import 'package:google_ml_kit/google_ml_kit.dart';
 import 'package:ocr_app_flutter/core/utils/result.dart';
 import 'package:ocr_app_flutter/image_processing/data/interfaces/image_processing_service.dart';
@@ -7,17 +8,31 @@ class ImageProcessingServiceImpl extends ImageProcessingService {
 
   ImageProcessingServiceImpl(this._textRecognizer);
 
+  // @override
+  // Future<Result<String, Exception>> processImage(String imagePath) async {
+  //   final InputImage inputImage = InputImage.fromFilePath(imagePath);
+  //   try {
+  //     final RecognizedText recognizedText = await _textRecognizer.processImage(
+  //       inputImage,
+  //     );
+  //     final String text = recognizedText.text;
+  //     return Result(value: text);
+  //   } on Exception catch (e) {
+  //     return Result(error: e);
+  //   }
+  // }
+
   @override
   Future<Result<String, Exception>> processImage(String imagePath) async {
-    final InputImage inputImage = InputImage.fromFilePath(imagePath);
     try {
-      final RecognizedText recognizedText = await _textRecognizer.processImage(
-        inputImage,
+      final String text = await FlutterTesseractOcr.extractText(
+        imagePath,
+        language: 'eng+spa',
+        args: {"preserve_interword_spaces": "1"},
       );
-      final String text = recognizedText.text;
       return Result(value: text);
-    } on Exception catch (e) {
-      return Result(error: e);
+    } catch (e) {
+      return Result(error: e as Exception);
     }
   }
 }
